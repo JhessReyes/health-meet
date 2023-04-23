689105f860cd9e95e0c44617567e4f179fa505bc
JhessReyes <jhessaureyes@gmail.com>
2023-04-22 23:26:40 -0600

    add tailwind

--- src/pages/schedule/index.jsx
+++ src/pages/schedule/index.jsx
@@ -4,7 +4,9 @@ import { FormSchedule, TableSchedule } from './_components'
 
 function index() {
     return (
-        <>
+        <>  <h1 class="text-3xl font-bold underline">
+            Hello world!
+        </h1>
             <Button onClick={(e) => test()} >Test</Button>
             <Button href='/schedule' >Test</Button>
             <Input placeholder="Basic usage" />
