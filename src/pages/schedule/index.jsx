ebc3e26af5c637d4f71bbcff7f0e909e7a5b44da
JhessReyes <jhessaureyes@gmail.com>
2023-04-23 00:28:33 -0600

    test

--- src/pages/schedule/index.jsx
+++ src/pages/schedule/index.jsx
@@ -4,14 +4,14 @@ import { FormSchedule, TableSchedule } from './_components'
 
 function index() {
     return (
-        <>  <h1 className="text-3xl font-bold underline">
-            Hello world!
-        </h1>
-            <Button onClick={(e) => test()} >Test</Button>
+        <>
+            <h1 className="text-3xl font-bold underline">
+                Hello world!
+            </h1>
             <Button href='/schedule' >Test</Button>
             <Input placeholder="Basic usage" />
             {/* <FormSchedule></FormSchedule> */}
-            <TableSchedule></TableSchedule>
+            {/*  <TableSchedule></TableSchedule> */}
         </>
     )
 }
