4596fea6f4850a0c86d02498b94897f0e3d6a92b
JhessReyes <jhessaureyes@gmail.com>
2023-05-01 18:39:46 -0600

    change methods

--- src/pages/schedule/_components/ScheduleModal.jsx
+++ src/pages/schedule/_components/ScheduleModal.jsx
@@ -1,25 +1,7 @@
-import { Button, message, Modal } from "antd";
-import { useEffect, useState } from "react";
+import { Modal } from "antd";
 import FormSchedule from "./FormSchedule";
-import { addDoc, collection, doc, setDoc } from "firebase/firestore";
-import { db } from "../../../database/firebase";
 
 const SchedulModal = (props) => {
-
-  /*   const addOrEdit = async (scheduleObject) => {
-      try {
-        setStatus(true);
-        const docRef = await addDoc(collection(db, "schedules"), { ...scheduleObject });
-        setStatus(false);
-        success();
-        setVisible(false);
-        console.log(docRef);
-        console.log('Successful')
-      } catch (error) {
-        console.error(error)
-      }
-    };
-   */
   return (
     <>
       <Modal
@@ -28,8 +10,7 @@ const SchedulModal = (props) => {
         name="Formulario de Horario"
         footer={[]}
       >
-        {/*         {contextHolder}
- */}        <FormSchedule addOrEdit={(schedule, isCreating) => props?.addOrEdit(schedule, isCreating)} cancel={(value) => props?.cancel(value)} status={props?.status} dataSchedule={props?.dataSchedule} />
+        <FormSchedule addOrEdit={(schedule, isCreating) => props?.addOrEdit(schedule, isCreating)} cancel={(value) => props?.cancel(value)} status={props?.status} dataSchedule={props?.dataSchedule} />
       </Modal>
     </>
   );
