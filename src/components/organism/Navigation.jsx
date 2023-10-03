a3954ca65dd8e74a0bacd7d6d27345c836642541
Jhonatan Essau Reyes Riveiro <jhessaureyes@gmail.com>
2023-10-03 16:03:48 -0600

    test

--- src/components/organism/Navigation.jsx
+++ src/components/organism/Navigation.jsx
@@ -3,7 +3,7 @@ import { HomeOutlined, ScheduleOutlined, SettingOutlined } from '@ant-design/ico
 import { Menu } from 'antd';
 import { useState } from 'react';
 import { NavLink } from 'react-router-dom';
-
+//test
 const items = [
     {
         label: (
